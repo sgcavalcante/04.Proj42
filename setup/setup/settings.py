f9ff682e26c48676a117f6a885d6c4cdff984b1b
Sergio Cavalcante <sgcdani@gmail.comc>
2023-10-05 21:39:30 -0300

    first commit

--- setup/setup/settings.py
+++ setup/setup/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = str(os.getenv('CHAVE_SECRETA'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
